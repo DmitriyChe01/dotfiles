3ce41021afad12de261b9108f82660521ed2fda7
DChe <happyproger01@gmail.com>
2024-07-02 12:42:52 +0300

    +

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  { "ellisonleao/gruvbox.nvim" },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+  },
+  {
+    "wilmanbarrios/palenight.nvim",
+  },
+  { "savq/melange-nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
